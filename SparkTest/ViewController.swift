3759e819bb86744ab5c45727353dceda19b38913
Dmitry Kanivets <dmitry.kanivets@uvoteam.com>
2018-06-28 13:39:41 +0300

    Created project

---
+++ SparkTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SparkTest
+//
+//  Created by Dmitry Kanivets on 6/28/18.
+//  Copyright © 2018 Dmitry Kanivets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
